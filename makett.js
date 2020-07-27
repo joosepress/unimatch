9400353d9a3f9528f0736710c8f163ef0eb6b716
Kuiugames <33493815+Kuiugames@users.noreply.github.com>
2020-07-28 01:24:46 +0300

    testing out back end ways (incase we have time to use it)

---
+++ makett.js
@@ -0,0 +1,27 @@
+//simple change script
+function change() {
+    document.getElementById("change").innerHTML = "Suurem";
+}
+
+//Gets "name" from test.json and changes id=test to that name
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        document.getElementById("test").innerHTML = myObj.name;
+    }
+};
+
+function test() {
+    xmlhttp.open("GET", "test.json", true);
+    xmlhttp.send();
+}
+
+//Adds element into id=addelement div, testing it so i could make new html elements with schools
+function addelement() {
+    var para = document.createElement("p");
+    var node = document.createTextNode(test());
+    para.appendChild(node);
+    var element = document.getElementById("addelement");
+    element.appendChild(para);
+}
