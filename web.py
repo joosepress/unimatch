515a0258ce5294b0ebe9425faa85549ebfa35049
Johannes Tammerand <johannestammmerand@gmail.com>
2020-07-28 22:13:24 +0300

    "Start Here" nupp viib teisele lehele

--- web.py
+++ web.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/') ##  kui baasurl on unifinder.com, siis unifinder.com/profile
+@app.route('/')
 def profile():
 	return render_template("makett.html")
+
+@app.route('/kusimustik/')
+def kusimustik():
+	return 'Vasta'
